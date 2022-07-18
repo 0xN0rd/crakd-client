1d0d950d83e12ed0c549d3a7b98015dcf20786c1
basisd3v <104164207+basisd3v@users.noreply.github.com>
2022-07-18 10:03:41 -0500

    pushing fix to a tag error

--- components/EnterTournament.js
+++ components/EnterTournament.js
@@ -3,6 +3,7 @@ import { ethers } from 'ethers';
 import LoadingIndicator from '../components/LoadingIndicator';
 import { CONTRACT_ADDRESS } from '../constants';
 import crakd from '../utils/CrakdTournament.json';
+import { chakra } from '@chakra-ui/react';
 
 const EnterTournament = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -129,9 +130,9 @@ const EnterTournament = () => {
                     </div>
                 </div>
             )}
-            <a href="/tournament">
+            <chakra.a href="/tournament">
                 <button type="button" className="go-button">Go to Tournament</button>
-            </a>
+            </chakra.a>
         </div>
     )
 }
